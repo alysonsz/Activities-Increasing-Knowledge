7c456192b466b31bfe42e911c3bf71fca2152ae4
Alyson Souza <125091371+alysonsz@users.noreply.github.com>
2024-04-08 13:52:20 -0300

    Create beecrowd - 1016.cs

---
+++ CSharp/beecrowd - 1016.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int distance;
+        distance = int.Parse(Console.ReadLine());
+        int result = distance*2;
+        Console.WriteLine("{0} minutos",result);
+    }
+
+}

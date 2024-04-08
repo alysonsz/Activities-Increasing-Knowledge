46bf71bd2d21444da8f05bccb3287f2c607e6666
Alyson Souza <125091371+alysonsz@users.noreply.github.com>
2024-04-08 14:00:27 -0300

    Create beecrowd - 1052.cs

---
+++ CSharp/beecrowd - 1052.cs
@@ -0,0 +1,49 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int month = int.Parse(Console.ReadLine());
+        if (month == 1){
+            Console.WriteLine("January");
+        }
+        else if (month == 2){
+            Console.WriteLine("February");
+        }
+        else if (month == 2){
+            Console.WriteLine("February");
+        }
+        else if (month == 3){
+            Console.WriteLine("March");
+        }
+        else if (month == 4){
+            Console.WriteLine("April");
+        }
+        else if (month == 5){
+            Console.WriteLine("May");
+        }
+        else if (month == 6){
+            Console.WriteLine("June");
+        }
+        else if (month == 7){
+            Console.WriteLine("July");
+        }
+        else if (month == 8){
+            Console.WriteLine("August");
+        }
+        else if (month == 9){
+            Console.WriteLine("September");
+        }
+        else if (month == 10){
+            Console.WriteLine("October");
+        }
+        else if (month == 11){
+            Console.WriteLine("November");
+        }
+        else if (month == 12){
+            Console.WriteLine("December");
+        }
+    }
+
+}

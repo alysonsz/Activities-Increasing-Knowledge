ec112a00f68f8113b968627d8db5f8c7b36330c3
Alyson Souza <125091371+alysonsz@users.noreply.github.com>
2024-04-08 14:04:00 -0300

    Create beecrowd - 1095.cs

---
+++ CSharp/beecrowd - 1095.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int j = 60;
+        for (int i = 1; j != -5; i = i + 3){
+            Console.WriteLine("I={0} J={1}", i, j);
+            j = j - 5;
+        }
+
+    }
+
+}
